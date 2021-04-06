4e7b2259fa1ef9ab9d5aa33bf82a3abcbd2f9cd1
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-04-06 16:34:15 +0100

    Initial commit.

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    # TODO: route to home page and logout
+    return render(request, 'accounts/signup.html')
