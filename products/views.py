8a1fe95570c66d38fb515cf325950d843ed31d37
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-04-07 16:34:47 +0100

    Created home page

--- products/views.py
+++ products/views.py
@@ -5,10 +5,11 @@ from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {"products": products})
 
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if _all_required_fields_present(request):
@@ -27,6 +28,7 @@ def detail(request, product_id):
     return render(request, 'products/detail.html', {"product": product})
 
 
+@login_required(login_url="/accounts/signup")
 def upvote(request, product_id):
     if request.method == 'POST':
         product = get_object_or_404(Product, pk=product_id)
