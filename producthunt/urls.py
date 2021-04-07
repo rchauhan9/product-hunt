e2f039a42dd0353d279e3ad92da5f036867749f8
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-04-07 16:09:28 +0100

    Added product creation and upvoting

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from products import views
 
@@ -22,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
